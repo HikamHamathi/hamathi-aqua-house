6cb4635efd279349312c7de73d5eb8ee1ac3b5cc
HikamHamathi <hikamfreelancing@gmail.com>
2026-07-02 22:48:24 +0530

    1 navbar & them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div style={{ padding: "30px" }}>
-      <h1>🐠 Hamathi Aqua House</h1>
-      <h2>React Working Successfully ✅</h2>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
