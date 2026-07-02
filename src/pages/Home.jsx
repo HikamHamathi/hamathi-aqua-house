2a45b8578d4c9d50c6e96a857c9e62bf5c1571b4
HikamHamathi <hikamfreelancing@gmail.com>
2026-07-02 23:41:51 +0530

    Product card & Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import ProductSection from "../components/ProductSection";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+        <ProductSection />
     </>
   );
 }
