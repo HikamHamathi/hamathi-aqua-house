608bf335cd43b84cf81aee208835aad935ec9119
HikamHamathi <hikamfreelancing@gmail.com>
2026-07-02 23:01:52 +0530

    1 navbar & them

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-
 function Home() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
+    <h1 style={{ color: "white", padding: "20px" }}>
+      Home Page Working
+    </h1>
   );
 }
 
 export default Home;
+
+// import Navbar from "../components/Navbar";
+// import Hero from "../components/Hero";
+
+// function Home() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//     </>
+//   );
+// }
+
+// export default Home;
