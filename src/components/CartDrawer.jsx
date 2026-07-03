4bbb0a385b0a53ee9964502d22ce6aa26f72f0fa
HikamHamathi <hikamfreelancing@gmail.com>
2026-07-03 14:26:55 +0530

    Search & Them & Card no

---
+++ src/components/CartDrawer.jsx
@@ -0,0 +1,15 @@
+function CartDrawer() {
+  return (
+    <div className="cart-drawer">
+      <h2>Shopping Cart</h2>
+
+      <p>Your cart items will appear here.</p>
+
+      <button className="checkout-btn">
+        Checkout
+      </button>
+    </div>
+  );
+}
+
+export default CartDrawer;
