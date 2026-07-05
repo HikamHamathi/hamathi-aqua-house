69ddee5dfe7d4f93b46161f0dfc9e48797a09060
HikamHamathi <hikamfreelancing@gmail.com>
2026-07-05 17:21:10 +0530

    Add to Cart

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,7 +11,21 @@ export function CartProvider({ children }) {
   const closeCart = () => setIsCartOpen(false);
 
   const addToCart = (product) => {
-    setCart((prev) => [...prev, product]);
+  setCart((prev) => {
+    const existing = prev.find((item) => item.id === product.id);
+
+    if (existing) {
+      return prev.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+    }
+
+    return [...prev, { ...product, quantity: 1 }];
+  });
+
+  setIsCartOpen(true);
   };
   const removeFromCart = (id) => {
   setCart((prev) => prev.filter((item) => item.id !== id));
